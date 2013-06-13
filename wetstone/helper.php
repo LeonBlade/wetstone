b754af7c461d527c3e68615d1d56281de6aed4ab
James Stine <leon.blade@gmail.com>
2013-06-12 23:37:56 -0400

    Add a better curl method

--- wetstone/helper.php
+++ wetstone/helper.php
@@ -19,6 +19,23 @@ function ris($var, $default = null) {
 	return (isset($var)) ? $var : $default;
 }
 
+function better_curl($url, $options = null, $json = false) {
+	// initialize the cURL handle
+	$ch = curl_init($url);
+	// set the cURL option for return transfer
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	// if the options array isn't null
+	if (!is_null($options) && is_array($options))
+		curl_setopt_array($ch, $options);
+	// go ahead and grab the execution now
+	$return = curl_exec($ch);
+	// close the handle
+	curl_close($ch);
+
+	// return the execution return JSON or not
+	return ($json) ? json_decode($return, true) : $return;
+}
+
 function php_errors() {
 	ini_set('display_errors', 1); 
  	error_reporting(E_ALL);
